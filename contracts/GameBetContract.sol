2eaa69ab324abeddbbf127764b8e859cf83117de
Vladimir <intfloatbool@gmail.com>
2019-12-11 12:31:54 +0300

    gameBet contract base

---
+++ contracts/GameBetContract.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.5.8;
+
+//import "@openzeppelin/contracts/payment/PaymentSplitter.sol";
+//import "@openzeppelin/contracts/access/roles/SignerRole.sol";
+
+contract GameBetContract is PaymentSplitter, SignerRole { 
+
+}
